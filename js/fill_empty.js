5e022adcff4472eb8b0f7d058dfc0148014ac3ce
Felix Buehler <account@buehler.rocks>
2020-04-02 01:32:33 +0200

    [js] empty listings now contain a minus instead of nothing

---
+++ js/fill_empty.js
@@ -0,0 +1,13 @@
+function fill_empty_listings(){
+	var listing_divs = document.getElementsByClassName("listing");
+	for(var i = 0; i < listing_divs.length; i++) {
+		listing_div = listing_divs[i];
+		var text = [].reduce.call(listing_div.childNodes, function(a, b) { return a + (b.nodeType === 3 ? b.textContent : ''); }, '');
+		var clean_text = text.replace(/\s/g,'');
+		if (clean_text === "") {
+			if (listing_div.getElementsByTagName("li").length == 0) {
+				listing_div.innerHTML += "—";
+			}
+		}
+	}
+}
