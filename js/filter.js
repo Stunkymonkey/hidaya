49ea1099959319133c6761a9e5615b6131efda0a
Felix Buehler <account@buehler.rocks>
2020-06-07 23:43:02 +0200

    made additive filter instead of string filter

--- js/filter.js
+++ js/filter.js
@@ -1,17 +1,22 @@
 function filter() {
 	var input, filter, ul, li, a, i, txtValue;
 	input = document.getElementById('filter_search');
-	filter = input.value.toUpperCase();
+	filter = input.value.toLowerCase();
 	ul = document.getElementsByClassName("media-container")[0];
 	li = ul.getElementsByTagName('li');
 
 	for (i = 0; i < li.length; i++) {
 		a = li[i].getElementsByTagName("a")[0];
 		txtValue = a.textContent || a.innerText;
-		if (txtValue.toUpperCase().indexOf(filter) > -1) {
-			li[i].style.display = "";
-		} else {
-			li[i].style.display = "none";
+		filter_values = filter.split(" ");
+		for (var j = filter_values.length - 1; j >= 0; j--) {
+			filter_word = filter_values[j];
+			if (txtValue.toLowerCase().indexOf(filter_word) > -1) {
+				li[i].style.display = "";
+			} else {
+				li[i].style.display = "none";
+				break;
+			}
 		}
 	}
 }
